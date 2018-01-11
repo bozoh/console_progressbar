3470ec0c24adeef9ec5568d20745df698d58fcf2
Carlos Alexandre S. da Fonseca <bozohhot@hotmail.com>
2018-01-11 12:09:10 -0200

    Send to pypi repo

---
+++ console_progressbar/progressbar.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+'''
+    Simple progress bar for console
+'''
+class ProgressBar(object):
+    """
+    Create terminal progress bar
+    @params:
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+    """
+    def __init__(self, total, prefix='', suffix='', decimals=1, length=100, fill='█'):
+        self.__prefix = prefix
+        self.__suffix = suffix
+        self.__decimals = decimals
+        self.__length = length
+        self.__fill = fill
+        self.__total = total
+
+    def generate_pbar(self, iteration):
+        """
+        Create and return the progress bar string
+        @params:
+            iteration   - Required  : current iteration (Int)
+        """
+        percent = ("{0:." + str(self.__decimals) + "f}")
+        percent = percent.format(100 * (iteration / float(self.__total)))
+        filled_length = int(self.__length * iteration // self.__total)
+        pbar = self.__fill * filled_length + '-' * (self.__length - filled_length)
+        return '{0} |{1}| {2}% {3}'.format(self.__prefix, pbar, percent, self.__suffix)
+
+    def print_progress_bar(self, iteration):
+        """
+        Prints the progress bar
+        @params:
+        iteration   - Required  : current iteration (Int)
+        """
+        print('\r%s' % (self.generate_pbar(iteration)), end='')
+        # Print New Line on Complete
+        if iteration == self.__total:
+            print()
--- setup.py
+++ setup.py
@@ -5,9 +5,8 @@ https://packaging.python.org/en/latest/distributing.html
 https://github.com/pypa/sampleproject
 """
 
-# Always prefer setuptools over distutils
+
 from setuptools import setup, find_packages
-# To use a consistent encoding
 from codecs import open
 from os import path
 from importlib import import_module
@@ -23,80 +22,27 @@ setup(
     version=import_module('console_progressbar').__version__,  
     description='A simple progress bar for console',
     long_description=long_description,
-    url='https://github.com/bozohrj/console-progressbar',
+    url='https://github.com/bozoh/console_progressbar',
     author='Carlos Alexandre S. da Fonseca',
     author_email='carlos.alexandre@outlook.com',
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
     license = 'MIT',
     keywords='progressbar console',
+    python_requires='>=3.5',
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
-    classifiers=[  # Optional
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
+    classifiers=[
         'Development Status :: 5 - Production/Stable',
-
-        # Indicate who your project is intended for
+        'Environment :: Console',
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-
-        # Pick your license as you wish
         'License :: OSI Approved :: MIT License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-    ],
-
-   
-
-    # This field lists other packages that your project depends on to run.
-    # Any package you put here will be installed by pip when your project is
-    # installed, so they must be valid existing projects.
-    #
-    # For an analysis of "install_requires" vs pip's requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    install_requires=[],  # Optional
-
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). Users will be able to install these using the "extras"
-    # syntax, for example:
-    #
-    #   $ pip install sampleproject[dev]
-    #
-    # Similar to `install_requires` above, these must be valid existing
-    # projects.
-    extras_require={  # Optional
+        'Topic :: Utilities'],
+    extras_require={
         'dev': ['check-manifest'],
         'test': ['coverage'],
     },
-
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.
-    #
-    # If using Python 2.6 or earlier, then these have to be included in
-    # MANIFEST.in as well.
-    package_data={},  # Optional
-        
-
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
-    #
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    data_files=[],  # Optional
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # `pip` to create the appropriate form of executable for the target
-    # platform.
-    #
-    # For example, the following would provide a command called `sample` which
-    # executes the function `main` from this package when invoked:
-    entry_points={},
 )
