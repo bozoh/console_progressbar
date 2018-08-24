c5bfc4411803c9ba3e56856dd459d48569391813
Carlos Alexandre S. da Fonseca <bozohhot@hotmail.com>
2018-08-23 23:35:53 -0300

    update tests

--- tests/test_progressbar.py
+++ tests/test_progressbar.py
@@ -1,6 +1,6 @@
 # the inclusion of the tests module is not meant to offer best practices for
 # testing in general, but rather to support the `find_packages` example in
-# setup.py that excludes installing the "tests" package
+# setup.py that excludes installing the 'tests' package
 from unittest import TestCase
 from console_progressbar import ProgressBar
 
@@ -14,21 +14,26 @@ class TestConsoleProgressBar(TestCase):
         self.assertIsInstance(pb,ProgressBar)
     
     def test_it_has_custom_prefix(self):
-        pb = ProgressBar(self.total, prefix="Teste")
-        self.assertTrue("Teste" in pb.generate_pbar(50))
+        pb = ProgressBar(self.total, prefix='Test')
+        self.assertTrue('Test' in pb.generate_pbar(50))
         
     
     def test_it_has_custom_suffix(self):
-        pb = ProgressBar(self.total, suffix="Teste")
-        self.assertTrue("Teste" in pb.generate_pbar(50))
+        pb = ProgressBar(self.total, suffix='Test')
+        self.assertTrue('Test' in pb.generate_pbar(50))
     
     def test_it_has_custom_fill(self):
-        pb = ProgressBar(self.total, fill="X")
-        self.assertTrue("X" in pb.generate_pbar(50))
+        pb = ProgressBar(self.total, fill='X')
+        self.assertTrue('X' in pb.generate_pbar(50))
+    
+    def test_it_has_custom_zero_fill(self):
+        pb = ProgressBar(self.total, zfill='=')
+        self.assertTrue('=' in pb.generate_pbar(50))
+        self.assertEqual(50, pb.generate_pbar(50).count('='))
     
     def test_it_has_custom_bar_length(self):
         pb = ProgressBar(self.total, length = 10)
-        # at 0% should be only 50 '-'
+        # at 0% should be only 10 '-'
         self.assertEqual(10, pb.generate_pbar(0).count('-'))
         # at 10% should be only 9 '-'
         self.assertEqual(9, pb.generate_pbar(10).count('-'))
@@ -39,18 +44,18 @@ class TestConsoleProgressBar(TestCase):
         # at 100% should be only 0 '-'
         self.assertEqual(0, pb.generate_pbar(100).count('-'))
     
-    def test_it_has_custom_decimals_palces(self):
+    def test_it_has_custom_decimals_places(self):
         pb = ProgressBar(98, decimals = 1)
         # 100*42/98 = 42,85714285714285714286
-        self.assertRegexpMatches(pb.generate_pbar(42),'\.\d{1}\%')
-        self.assertNotRegexpMatches(pb.generate_pbar(42),'\.\d{2}\%')
-        self.assertNotRegexpMatches(pb.generate_pbar(42),'\.\d{3}\%')
+        self.assertRegexpMatches(pb.generate_pbar(42),r'\.\d{1}\%')
+        self.assertNotRegexpMatches(pb.generate_pbar(42),r'\.\d{2}\%')
+        self.assertNotRegexpMatches(pb.generate_pbar(42),r'\.\d{3}\%')
         pb = ProgressBar(98, decimals = 2)
-        self.assertNotRegexpMatches(pb.generate_pbar(42),'\.\d{1}\%')
-        self.assertRegexpMatches(pb.generate_pbar(42),'\.\d{2}\%')
-        self.assertNotRegexpMatches(pb.generate_pbar(42),'\.\d{3}\%')
+        self.assertNotRegexpMatches(pb.generate_pbar(42),r'\.\d{1}\%')
+        self.assertRegexpMatches(pb.generate_pbar(42),r'\.\d{2}\%')
+        self.assertNotRegexpMatches(pb.generate_pbar(42),r'\.\d{3}\%')
         pb = ProgressBar(98, decimals = 3)
-        self.assertNotRegexpMatches(pb.generate_pbar(42),'\.\d{1}\%')
-        self.assertNotRegexpMatches(pb.generate_pbar(42),'\.\d{2}\%')
-        self.assertRegexpMatches(pb.generate_pbar(42),'\.\d{3}\%')
+        self.assertNotRegexpMatches(pb.generate_pbar(42),r'\.\d{1}\%')
+        self.assertNotRegexpMatches(pb.generate_pbar(42),r'\.\d{2}\%')
+        self.assertRegexpMatches(pb.generate_pbar(42),r'\.\d{3}\%')
 
