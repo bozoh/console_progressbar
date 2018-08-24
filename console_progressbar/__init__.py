18631f625e6e7ef5b39c8391b7b4aaea9f7af2bd
Carlos Alexandre S. da Fonseca <bozohhot@hotmail.com>
2018-08-23 23:50:33 -0300

    v1.0.1

--- console_progressbar/__init__.py
+++ console_progressbar/__init__.py
@@ -7,8 +7,8 @@ from console_progressbar.progressbar import ProgressBar
 
 
 __author__ = 'Carlos Alexandre S. da Fonseca'
-__version__ = '1.0.1'
-__date__ = '2018-01-11'  # YYYY-MM-DD
+__version__ = '1.1.0'
+__date__ = '2018-08-23'  # YYYY-MM-DD
 
 _DEBUG_MODE = False
 
