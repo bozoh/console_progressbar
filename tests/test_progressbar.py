6e194787f170eea47db4dc564cb5ee09a7b7351d
Carlos Alexandre S. da Fonseca <bozoh@users.noreply.github.com>
2025-01-02 17:39:33 -0300

    Update test_progressbar.py

--- tests/test_progressbar.py
+++ tests/test_progressbar.py
@@ -1,7 +1,7 @@
 # the inclusion of the tests module is not meant to offer best practices for
 # testing in general, but rather to support the `find_packages` example in
 # setup.py that excludes installing the 'tests' package
-from mock import patch, Mock, MagicMock
+# from mock import patch, Mock, MagicMock
 from unittest import TestCase
 from console_progressbar import ProgressBar
 
